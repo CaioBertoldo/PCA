02b1b9f7326ce6a6589b03da25693f8538bd722d
CaioBertoldo <120601475+CaioBertoldo@users.noreply.github.com>
2023-03-07 17:58:09 -0400

    Update 20230307_001.c

--- 20230307_001.c
+++ 20230307_001.c
@@ -1,4 +1,3 @@
-# PCA
 /*Escreva uma função que imprime os elementos de um vetor de float a partir de um endereço inicial (ponteiro) até um endereço final;
  A função recebe como parâmetros dois ponteiros para float (os endereços inicial e final) e deve utilizar notação de ponteiros não de vetores;
 - Deve ser criada uma função main() para testar a função implementada com três vetores de tamanhos e conteúdos diferentes;
@@ -8,6 +7,8 @@
 #include <stdlib.h>
 
 void print_vet(float *start_adress, float *final_adress){
+
+    // printar o vetor
     for (; start_adress <= final_adress; start_adress++)
     {
         printf("%.1f\n", *start_adress);
@@ -17,12 +18,35 @@ void print_vet(float *start_adress, float *final_adress){
 
 int main(){
     float vector1[5] = {1.0, 2.0, 3.0, 4.0, 5.0,};
-    float vector2[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
-    float vector3[5] = {1.2, 2.3, 3.4, 4.5, 5.6};
+    float vector2[5] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7};
+    float vector3[5] = {1.2, 2.3, 3.4, 4.5};
 
+    // Imprimindo os vetores completos
     print_vet(&vector1[0], &vector1[4]);
     printf("\n--------------\n");
-    print_vet(&vector2[0], &vector2[4]);
+    print_vet(&vector2[0], &vector2[6]);
     printf("\n--------------\n");
-    print_vet(&vector3[0], &vector3[4]);
+    print_vet(&vector3[0], &vector3[3]);
+    printf("\n--------------\n");
+
+    //Imprimir o vetor 1 por partes
+    print_vet(&vector1[0], &vector1[1]);
+    printf("\n--------------\n");
+
+    print_vet(&vector1[2], &vector1[4]);
+    printf("\n--------------\n");
+
+    //Imprimir o vetor 2 por partes
+    print_vet(&vector2[0], &vector2[3]);
+    printf("\n--------------\n");
+
+    print_vet(&vector2[3], &vector2[6]);
+    printf("\n--------------\n");
+
+    //Imorimir o vetor 3 por partes
+    print_vet(&vector3[0], &vector3[1]);
+    printf("\n--------------\n");
+
+    print_vet(&vector3[2], &vector3[3]);
+
 }
